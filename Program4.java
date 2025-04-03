247ed43094382935c70540e6f06d795aa31e0a88
shaikmohammedabdulazeez <shaikmohammedabdulazeez@gmail.com>
2025-04-04 02:13:45 +0530

    uploading java notes

---
+++ Program4.java
@@ -0,0 +1,69 @@
+
+class Program4 {
+    public static void main(String[] args) {
+        System.out.println(456);
+        System.out.println('d');
+        System.out.println("java");
+        System.out.println(true);
+        
+        System.out.println("============================");
+        
+         String conatiner1 ="am learning java";
+         
+         
+         System.out.println(conatiner1);
+         System.out.println(conatiner1);
+         System.out.println(conatiner1);
+         System.out.println(conatiner1);
+         System.out.println(conatiner1);
+         System.out.println(conatiner1);
+         
+        System.out.println("-----------------------------");
+        
+            String value1="hi buddies";
+            int value2 =10;
+            double value3 =345.00;
+            char value4 ='x';
+            boolean value5=false;
+            
+            System.out.println("+++++++++++++++++++++++++");
+            int a=40;
+            int b=10;
+            String c="java";
+            System.out.println(a+b);//50
+            System.out.println(a-b);//30
+            System.out.println(a*b);//400
+            System.out.println(a/b);//4
+            System.out.println(a%b);//0
+            System.out.println(a+c+b);
+                              //50java10
+            System.out.println(c+b+a);
+                              //java1040
+            System.out.println(a+b+c);
+                              //40+10+java
+                              //50java
+            
+            
+        
+        
+
+    }
+}
+
+
+/*
+Number data type-Interger data type 1,100,3445,3489654
+                -Decimal data type 140.02 , 500.33, 20.00
+        Integer:
+              byte, short , int ,long
+        Decimal :
+              double , float
+ 
+character datatype-a, b, c, $,
+                  -it need to annotate with singlequote(' ');
+                  
+String datatype - banana , apple , apple12 , red#blue
+                -collection of characters
+                - it need to annotate with doublequote(" ")
+boolean datatype -true /false
+*/
