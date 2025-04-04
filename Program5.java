6296294fecf2e6254ad279536fd9790661510d60
shaikmohammedabdulazeez <shaikmohammedabdulazeez@gmail.com>
2025-04-04 22:38:42 +0530

    uploading notes

---
+++ Program5.java
@@ -0,0 +1,45 @@
+class Program5{
+  public static void main(String[]args){
+   System.out.println("java session started");
+   System.out.println("this is my work "+500);
+      int a=10;
+      int b=20;
+  System.out.println(a<b);//10<20//true
+  System.out.println(a>b);//10>20//false
+  System.out.println(a==b);//10==20//false
+  System.out.println(a!=b);//10!=20//true
+  System.out.println(a<=b);//10<=20//true
+  System.out.println(a>=b);//10>=20//true
+
+
+
+
+
+
+}
+
+}
+
+/*
+compilation --javac FileName.java(check syntax)
+excecution --java className/FileName.java (main method is mandatory)
+
+
+operations/task
+   -- to perform we need to operators(+,-....)
+
+1.Arthitmetic operator
+   -- (+,-,*,/,%)
+2.Comparision/Relational operator
+     - return boolean value(true/false)
+     -(<-lessthan, >greaterthan ,==equalsto , !=not equal, <=lessthanorequal , >=greaterthanorequal)
+
+
+3.Logical operator
+4.bitwise operator
+5.unary operator
+6.conditional/ternary operator
+7.Assignment operator
+
+
+*/
