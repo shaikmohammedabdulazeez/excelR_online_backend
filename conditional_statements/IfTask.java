0de93ad61ec02a0be67cbf85b77aaf7844d3dae5
shaikmohammedabdulazeez <shaikmohammedabdulazeez@gmail.com>
2025-04-10 00:02:48 +0530

    uploading code

---
+++ conditional_statements/IfTask.java
@@ -0,0 +1,27 @@
+
+class IfTask {
+    public static void main(String[] args) {
+        
+        int age=2;
+
+        if(age>=18){
+            System.out.println("you can vote");
+             System.out.println("you can drive");
+              System.out.println("you can marry");
+        }
+
+
+
+        if(true){
+            System.out.println("hello");
+        }
+           if(false){
+            System.out.println("hii");
+        }
+
+
+
+
+
+    }
+}
